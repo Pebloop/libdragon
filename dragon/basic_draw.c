82156a2a4f6e01e93ef01e062a3ff54491bf085c
Tempo <alexleg33@outlook.fr>
2019-10-30 01:53:07 +0100

    feat: add draw_rectangle

--- dragon/basic_draw.c
+++ dragon/basic_draw.c
@@ -30,3 +30,12 @@ void draw_square(framebuffer_t *fb, sfVector2u position,
         for (int j = position.y; j < position.y + size; j++)
             put_pixel_in_fb(fb, i, j, color);
 }
+
+//draw a rectangle
+void draw_rectangle(framebuffer_t *fb, sfVector2u position,
+                 sfVector2u size, sfColor color)
+{
+    for (int i = position.x; i < position.x + size.x; i++)
+        for (int j = position.y; j < position.y + size.y; j++)
+            put_pixel_in_fb(fb, i, j, color);
+}
